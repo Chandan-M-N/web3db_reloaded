85d25f768d64070ec6d6579c7cc995a2b3902b16
Showkot Hossain <showkot.csedu@gmail.com>
2025-03-03 15:26:13 -0500

    access control smart contract added

---
+++ access_control/contracts/AccessControl.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract AccessControl {
+
+    // Mapping from resource ID to permitted user identifiers
+    mapping(string => mapping(string => bool)) public permissions;
+    
+    // Events
+    event PolicyAdded(string resourceId, string subscriberId);
+    event PolicyEvaluated(string resourceId, string requesterId, bool authorized);
+
+    // Function to add policy (share access)
+    function addPolicy(
+        string memory resourceId,
+        string memory subscriberId
+    ) public {
+        permissions[resourceId][subscriberId] = true;
+        emit PolicyAdded(resourceId, subscriberId);
+    }
+
+    // Function to evaluate policy (check access)
+    function evaluatePolicy(
+        string memory resourceId,
+        string memory requesterId
+    ) public view returns (bool) {
+        return permissions[resourceId][requesterId];
+    }
+}
